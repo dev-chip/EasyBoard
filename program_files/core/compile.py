668316f99e3f49dc89904363bed6fc3abda08df1
Chip <chip.ck.main@gmail.com>
2020-10-30 21:03:15 +0000

    Copy header files used for compilation into the CRAM folder.

--- program_files/core/compile.py
+++ program_files/core/compile.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import io
+import re
+import shutil
 
 from tools.logger import init_console_logger
 
@@ -13,9 +15,39 @@ def cosmic_compile(c_file_path, logger=init_console_logger(name="cosmic_compile"
     """
         Compiles using the cosmic compiler.
     """
+    # checks
     assert(os.path.isfile(c_file_path))
     assert(".c" == c_file_path[-2:])
     logger.info("Compiling...")
+
+    # clean old header files
+    logger.info("Cleaning old header files...")
+    previous_h_files = [_ for _ in os.listdir(CRAM_DIR_PATH) if _[-2:] == ".h"]
+    for f in previous_h_files:
+        try:
+            os.remove(os.path.join(CRAM_DIR_PATH, f))
+        except Exception as e:
+            logger.warning("Failed to clean old header files: '{}' could not be deleted: {}".format(f, e))
+
+    # collect source header files
+    with open(c_file_path, "r") as f:
+        lines = f.read()
+    local_includes = re.findall(r"#include\s{0,1}\".+[^\s]\"", lines)
+    h_filenames = [_[8:].replace('"', '').replace(' ', '') for _ in local_includes]
+
+    # copy source header files
+    source_dir_path = os.path.join(os.path.dirname(c_file_path))
+    for h_filename in h_filenames:
+        src = os.path.join(source_dir_path, h_filename)
+        dst = os.path.join(CRAM_DIR_PATH, h_filename)
+        try:
+            logger.info("Copying header file '{}' to '{}' ...".format(src, dst))
+            shutil.copyfile(src, dst)
+        except IOError as e:
+            logger.error("Header file '{}' was not found. Compilation failed. Error message: ".format(src, e))
+            return
+
+    # compile
     command = r'"{}" "{}" "{}"'.format(COMPILE_BAT_PATH, CRAM_DIR_PATH, c_file_path)
     logger.debug("Executing command: {}".format(command))
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
